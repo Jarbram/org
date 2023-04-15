9e1e3101a4a9a127323f297d04ae25a1989ab875
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-04-14 23:00:38 -0600

    change the last routes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(img/footer.png)" }}>
         <div className='redes'>
             <a href='https://www.facebook.com/abraham.huacchillo/'>
-                <img src="/img/facebook.png" alt='Facebook' />
+                <img src="img/facebook.png" alt='Facebook' />
             </a>
             <a href='https://www.instagram.com/abrahamhuacchillo/'>
                 <img src="img/instagram.png" alt='instagram' />
             </a>
         </div>
-        <img src='/img/Logo.png' alt='org' />
+        <img src='img/Logo.png' alt='org' />
         <p>Desarrollado por Jarbram</p>
     </footer>
 }
